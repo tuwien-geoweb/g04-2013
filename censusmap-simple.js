a4df03aed48bd37aa31ca1479a30d25de5f917ab
Thomas Feilmayr <thomas.feilmayr@gmx.at>
2013-11-21 14:51:10 +0100

    Update censusmap-simple.js

--- censusmap-simple.js
+++ censusmap-simple.js
@@ -89,6 +89,11 @@ form.onsubmit = function(evt) {
   xhr.send();
   evt.preventDefault();
 };
-    
 
+// View für kontrollierten Zoom bei Adressenaufruf    
+view: new ol.View2D({
+  center: [-10764594.0, 4523072.0],
+  zoom: 5,
+  maxZoom: 18
+})
 
